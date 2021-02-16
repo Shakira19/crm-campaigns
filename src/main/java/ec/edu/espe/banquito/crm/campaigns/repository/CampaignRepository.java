1a82f33dfede80390db3ed730165c2cf09bb8533
Shakira19 <kscofre@espe.edu.ec>
2021-02-16 03:03:50 -0500

    Created the repositories and specified the db behavior to update

---
+++ src/main/java/ec/edu/espe/banquito/crm/campaigns/repository/CampaignRepository.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.banquito.crm.campaigns.repository;
+
+import ec.edu.espe.banquito.crm.campaigns.model.Campaign;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author cofre
+ */
+public interface CampaignRepository extends JpaRepository<Campaign, Integer> {
+    List<Campaign> findAll();
+    Campaign findById();
+}
---
+++ src/main/java/ec/edu/espe/banquito/crm/campaigns/repository/ContactabilityRegistrationRepository.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.banquito.crm.campaigns.repository;
+
+import ec.edu.espe.banquito.crm.campaigns.model.ContactabilityRegistration;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author cofre
+ */
+
+
+public interface ContactabilityRegistrationRepository extends JpaRepository<ContactabilityRegistration, Integer> {
+    List<ContactabilityRegistration> findAll();
+    ContactabilityRegistration findById();
+}
