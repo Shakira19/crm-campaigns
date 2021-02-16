9ac5c79e478dafc9f159f76307483e3ee7b1ee38
Shakira19 <kscofre@espe.edu.ec>
2021-02-16 00:57:09 -0500

    Creation of the project

---
+++ src/test/java/ec/edu/espe/banquito/crm/campaigns/CrmCampaignsApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.banquito.crm.campaigns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmCampaignsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
