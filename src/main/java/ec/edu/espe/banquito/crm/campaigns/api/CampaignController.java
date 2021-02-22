2f5342b82fbddabab452e9883a917ee10c7f4a37
Alan Quimbita <adquimbita@espe.edu.ec>
2021-02-22 11:12:05 -0500

    Changed creation and edition of campaigns

--- src/main/java/ec/edu/espe/banquito/crm/campaigns/api/CampaignController.java
+++ src/main/java/ec/edu/espe/banquito/crm/campaigns/api/CampaignController.java
@@ -29,18 +29,17 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author cofre
  */
-
 @RestController
 @RequestMapping("/api/campaigns")
 @Slf4j
 public class CampaignController {
-    
+
     private final CampaignService service;
-    
+
     public CampaignController(CampaignService service) {
-        this.service = service;         
+        this.service = service;
     }
-    
+
     @GetMapping
     public ResponseEntity listarCampaigns() {
         try {
@@ -48,7 +47,7 @@ public class CampaignController {
 
             this.service.listarCampaigns().forEach(items::add);
 
-            if (items.isEmpty()){
+            if (items.isEmpty()) {
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             }
             return new ResponseEntity<>(items, HttpStatus.OK);
@@ -57,7 +56,7 @@ public class CampaignController {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-    
+
     @GetMapping("/{id}")
     public ResponseEntity obtenerCampaignPorId(@PathVariable Integer id) {
         try {
@@ -68,19 +67,25 @@ public class CampaignController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
-    
+
     @GetMapping("/byName/{name}")
-    public ResponseEntity getCampaignByName(@PathVariable String name){
+    public ResponseEntity getCampaignByName(@PathVariable String name) {
         log.info("The campaign that match it's name with {}, will be retrieved", name);
         return ResponseEntity.ok(this.service.getCampaignByName(name));
     }
-            
+
     @PostMapping
     public ResponseEntity crearCampaign(@RequestBody CampaignRQ campaign) {
         try {
+            log.info("A new campaign will be created: {}", campaign);
             this.service.crearCampaign(Campaign.builder()
-                                        .name(campaign.getName())
-                                        .description(campaign.getDescription()).build());
+                    .name(campaign.getName())
+                    .description(campaign.getDescription())
+                    .startDate(campaign.getStartDate())
+                    .endDate(campaign.getEndDate())
+                    .urlTermsConditions(campaign.getUrlTermsConditions())
+                    .region(campaign.getRegion())
+                    .build());
             return ResponseEntity.ok().build();
         } catch (InsertException e) {
             return ResponseEntity.badRequest().build();
@@ -88,22 +93,25 @@ public class CampaignController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
     }
-    
+
     @PutMapping("/{id}")
     public ResponseEntity editarCampaign(@PathVariable Integer id, @RequestBody CampaignRQ campaignRq) {
         try {
-            Campaign campaign = this.service.obtenerCampaignPorId(id);
-            campaign.setName(campaignRq.getName());
-            campaign.setDescription(campaignRq.getDescription());
-            this.service.editarCampaign(campaign, id);
+            this.service.editarCampaign(Campaign.builder()
+                    .name(campaignRq.getName())
+                    .description(campaignRq.getDescription())
+                    .startDate(campaignRq.getStartDate())
+                    .endDate(campaignRq.getEndDate())
+                    .urlTermsConditions(campaignRq.getUrlTermsConditions())
+                    .region(campaignRq.getRegion()).build(), id);
             return ResponseEntity.ok().build();
         } catch (RegistryNotFoundException e) {
-            return ResponseEntity.badRequest().build();
+            return ResponseEntity.notFound().build();
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
-    
+
     @PostMapping("/asignar-cliente/{id}")
     public ResponseEntity asignarCliente(@PathVariable Integer id, @RequestParam String clientId) {
         try {
@@ -115,7 +123,7 @@ public class CampaignController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
-    
+
     @PutMapping("/actualizar-contacto/{id}")
     public ResponseEntity actualizarContacto(@PathVariable Integer id, @RequestParam String status) {
         try {
--- src/main/java/ec/edu/espe/banquito/crm/campaigns/service/CampaignService.java
+++ src/main/java/ec/edu/espe/banquito/crm/campaigns/service/CampaignService.java
@@ -5,6 +5,7 @@
  */
 package ec.edu.espe.banquito.crm.campaigns.service;
 
+import ec.edu.espe.banquito.crm.campaigns.enums.CampaignStatusEnum;
 import ec.edu.espe.banquito.crm.campaigns.model.Campaign;
 import ec.edu.espe.banquito.crm.campaigns.repository.CampaignRepository;
 import ec.edu.espe.banquito.crm.campaigns.repository.ContactabilityRegistrationRepository;
@@ -14,79 +15,97 @@ import ec.edu.espe.banquito.crm.campaigns.exception.RegistryNotFoundException;
 import ec.edu.espe.banquito.crm.campaigns.model.ContactabilityRegistration;
 import ec.edu.espe.banquito.crm.campaigns.enums.ContactStatusEnum;
 import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 /**
  *
  * @author cofre
  */
-
 @Service
+@Slf4j
 public class CampaignService {
+
     private final CampaignRepository campaignRepo;
     private final ContactabilityRegistrationRepository contactabilityRegistrationRepo;
-    
+
     public CampaignService(CampaignRepository campaignRepo, ContactabilityRegistrationRepository contactabilityRegistrationRepo) {
         this.campaignRepo = campaignRepo;
         this.contactabilityRegistrationRepo = contactabilityRegistrationRepo;
     }
-    
+
     public List<Campaign> listarCampaigns() {
-        return this.campaignRepo.findAll(); 
+        return this.campaignRepo.findAll();
     }
-    
+
     public Campaign obtenerCampaignPorId(Integer id) throws RegistryNotFoundException {
         Optional<Campaign> campaign = this.campaignRepo.findById(id);
-        if(campaign.isPresent()) {
+        if (campaign.isPresent()) {
             return campaign.get();
         } else {
-            throw new RegistryNotFoundException("No existe una campaña con el id: "+id);
+            throw new RegistryNotFoundException("No existe una campaña con el id: " + id);
         }
     }
-    
+
     public void crearCampaign(Campaign campaign) throws InsertException {
-        if(this.campaignRepo.save(campaign) == null) {
-            throw new InsertException("campaign", "No se pudo insertar el registro, revise los datos.");
-        } 
+        try {
+            campaign.setStatus(CampaignStatusEnum.ACTIVE.getStatus());
+            campaign.setTotalNumberClients(0);
+            campaign.setNumberAssignedClients(0);
+            campaign.setNumberAccepetdClients(0);
+            campaign.setNumberClientsInProgress(0);
+            campaign.setNumberRejectedClients(0);
+            this.campaignRepo.save(campaign);
+            log.info("The campaign {}, was created", campaign);
+        } catch (Exception e) {
+            log.error("The campaign {} could not be created", campaign);
+            throw new InsertException("campaign", "The campaign {} could not be created");
+        }
     }
-    
+
     public void editarCampaign(Campaign campaign, Integer id) throws RegistryNotFoundException {
         Optional<Campaign> campaignToEdit = this.campaignRepo.findById(id);
-        if(campaignToEdit.isPresent()) {
+        if (campaignToEdit.isPresent()) {
             Campaign editedCampaign = campaignToEdit.get();
             editedCampaign.setName(campaign.getName());
             editedCampaign.setDescription(campaign.getDescription());
+            editedCampaign.setStartDate(campaign.getStartDate());
+            editedCampaign.setEndDate(campaign.getEndDate());
+            editedCampaign.setUrlTermsConditions(campaign.getUrlTermsConditions());
+            editedCampaign.setRegion(campaign.getRegion());
             this.campaignRepo.save(editedCampaign);
+            log.info("The campaign with id: {}, was edited", id);
         } else {
-            throw new RegistryNotFoundException("No existe la campaña que desea editar con id: "+id);
+            log.error("The campaign with id: {}, was not found to be edited", id);
+            throw new RegistryNotFoundException("The campaign with id:" + id + ", was not found to be edited");
         }
     }
-    
+
     public void asignarCliente(Integer id, String clientId) throws RegistryNotFoundException {
         Optional<Campaign> campaignToRegister = this.campaignRepo.findById(id);
-        if(campaignToRegister.isPresent()) {
+        if (campaignToRegister.isPresent()) {
             ContactabilityRegistration contactabilityRegistration = ContactabilityRegistration.builder()
-                                                                                              .campaign(campaignToRegister.get())
-                                                                                              .idClient(clientId)
-                                                                                              .status(ContactStatusEnum.PENDIENTE.getStatus()).build();
+                    .campaign(campaignToRegister.get())
+                    .idClient(clientId)
+                    .status(ContactStatusEnum.PENDIENTE.getStatus()).build();
             this.contactabilityRegistrationRepo.save(contactabilityRegistration);
         } else {
-            throw new RegistryNotFoundException("No se pudo encontrar una campaña con el id: "+id);
+            throw new RegistryNotFoundException("No se pudo encontrar una campaña con el id: " + id);
         }
     }
-    
-    public void actualizarContacto(Integer contactabilityId, ContactStatusEnum status) throws RegistryNotFoundException{
+
+    public void actualizarContacto(Integer contactabilityId, ContactStatusEnum status) throws RegistryNotFoundException {
         Optional<ContactabilityRegistration> contactabilityToUpdate = this.contactabilityRegistrationRepo.findById(contactabilityId);
-        if(contactabilityToUpdate.isPresent()) {
+        if (contactabilityToUpdate.isPresent()) {
             ContactabilityRegistration updatedContactability = contactabilityToUpdate.get();
             updatedContactability.setStatus(status.getStatus());
             this.contactabilityRegistrationRepo.save(updatedContactability);
         } else {
-            throw new RegistryNotFoundException("No se encontro un registro de contactabilidad con id: "+contactabilityId);
+            throw new RegistryNotFoundException("No se encontro un registro de contactabilidad con id: " + contactabilityId);
         }
     }
-    
-    public List<Campaign> getCampaignByName(String name){
+
+    public List<Campaign> getCampaignByName(String name) {
         return this.campaignRepo.findByNameLikeIgnoreCaseOrderByNameAsc(name);
     }
 }
