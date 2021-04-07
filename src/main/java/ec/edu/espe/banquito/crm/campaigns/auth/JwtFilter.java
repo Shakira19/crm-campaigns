f77cfb52baa88f8b5a8a8ea9a8d001b37f805a79
steevenm888 <steevenm888@gmail.com>
2021-04-07 03:42:30 -0500

    changes to allow preflight requests

--- src/main/java/ec/edu/espe/banquito/crm/campaigns/auth/JwtFilter.java
+++ src/main/java/ec/edu/espe/banquito/crm/campaigns/auth/JwtFilter.java
@@ -3,17 +3,20 @@ package ec.edu.espe.banquito.crm.campaigns.auth;
 import ec.edu.espe.banquito.crm.campaigns.service.JwtUserDetailsService;
 import io.jsonwebtoken.ExpiredJwtException;
 import java.io.IOException;
+import java.util.Enumeration;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
@@ -31,8 +34,19 @@ public class JwtFilter extends OncePerRequestFilter {
             throws ServletException, IOException {
 
         String tokenHeader = request.getHeader("Authorization");
+        log.debug(tokenHeader);
         String username = null;
         String token = null;
+        Enumeration<String> iterable = request.getHeaderNames();
+        MultiValueMap<String, String> headers = new HttpHeaders();
+        for (Enumeration names = request.getHeaderNames(); names.hasMoreElements();) {
+            String name = (String) names.nextElement();
+            for (Enumeration values = request.getHeaders(name); values.hasMoreElements();) {
+                String value = (String) values.nextElement();
+                log.info("key: {}, value: {}", name, value);
+                headers.add(name, value);
+            }
+        }
         if (tokenHeader != null && tokenHeader.startsWith("Bearer ")) {
             token = tokenHeader.substring(7);
             try {
@@ -43,7 +57,8 @@ public class JwtFilter extends OncePerRequestFilter {
                 log.error("JWT Token has expired");
             }
         } else {
-            log.error("Bearer String not found in token");
+
+            log.error("Bearer String not found in token {}", request.getHeader("access-control-request-headers"));
         }
         if (null != username && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = userDetailsService.loadUserByUsername(username);
